faf7ead5f9aac80cf6751e431f535249a7b755c7
tonissonm <116629360+tonissonm@users.noreply.github.com>
2025-12-01 17:20:57 +0200

    Lisasin juurde asju, nagu navbar ja carousel

--- style.ts
+++
@@ -1,6 +0,0 @@
-function Marquee(text:string, speed: number=100){
-    const Marqueetext = document.getElementById('marqueetext');
-    Marqueetext?.addEventListener('animationiteration', () => {
-        Marqueetext.style.animation = "none";
-        
-}
